91942c07fcf2444bdba36b2b473c680dc5a43feb
Bilal <alibilal.cto@gmail.com>
2026-02-24 20:19:44 +0500

    Updated rendition04 again to do more stuff on soon

--- Main_Rendition04.py
+++ Main_Rendition04.py
@@ -2,4 +2,6 @@
 TO-DO List
 - Make this a better version of the calculator code from the top
 - Basically also add a Class for the calculator object to make it so that it can run with the Calculator object in mind and not just a if name equals main thing anymore.
+- Make the code neater and cleaner, and if you can faster.
+- Make and see if the keyboard can work with it (i.e. backspace and number keys as well as operators).
 """

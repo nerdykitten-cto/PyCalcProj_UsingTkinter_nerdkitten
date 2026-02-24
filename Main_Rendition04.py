659599dfa535f046ac700f37d081dbcb3f3e1414
Bilal <alibilal.cto@gmail.com>
2026-02-24 19:47:50 +0500

    Updated Rendtion04 with more to-do things to do.

--- Main_Rendition04.py
+++ Main_Rendition04.py
@@ -1,5 +1,5 @@
 """
 TO-DO List
 - Make this a better version of the calculator code from the top
-
+- Basically also add a Class for the calculator object to make it so that it can run with the Calculator object in mind and not just a if name equals main thing anymore.
 """
